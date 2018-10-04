f167d33538f27d877c7e21375f2f0ef21b219458
David Wolf <wolfd4@hawkmail.newpaltz.edu>
2018-10-04 12:18:37 -0400

    Added Web Server Option

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 
 const math = require('./math');
+const express = require('express');
 
-var something = math.add(2, 2);
 
 console.log("2 + 2 = " + something);
+
+const app = express();
+
+app.get("/add", function(req, res,){
+    var something = math.add(2, 2);
+    res.send({something: something});
+});
+app.use("/", function(req, res, next){
+    res.send("Hello World");
+});
+
+app.listen(3000);
